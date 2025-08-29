55c92117b3758de141230251d78ee266ce1de819
kundan <kundanbhardwaj145@gmail.com>
2025-08-29 17:18:14 +0530

    second

--- TravelX/settings.py
+++ TravelX/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-25)#vx!-#t%wfg*0$i79l!j*3sn=oa%qw*(s@aykmv!2p+ba%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -83,10 +84,11 @@ WSGI_APPLICATION = 'TravelX.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config("DATABASE_URL"), 
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
